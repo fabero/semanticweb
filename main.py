16b7f13642bd6242082c6aa4fe8afd8ebd0c34d9
Lennart Faber <lennart@klippa.com>
2019-10-10 14:03:37 +0200

    Find hometown

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import argparse
+from Spotify import *
+from DBPedia import *
+
+dbpedia = DBPedia()
+spotify = Spotify()
 
 
 def main(parseargs):
-    print(parseargs.query)
+    # @todo: Convert query to correct DBpedia form (e.g. green day -> Green_Day)
+    artist = parseargs.query
+
+    hometown = dbpedia.get_hometown(artist)
+    print(hometown)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fetch data on musical artists and bands.')
-    parser.add_argument('query',  type=str, nargs='+',
-                        help='The band or artist to search for.')
+    parser.add_argument('query',  type=str, help='The band or artist to search for.')
     args = parser.parse_args()
     main(args)
